096a767eec52925ddcb28dfb58e69c0a4884ef74
fatih <fsenyigit8@gmail.com>
2024-03-09 15:12:35 +0300

    inout form

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
-const salary = document.querySelector("#income");
-const ekle = document.querySelector(".btn-green");
-const kaydet = document.querySelector(".btn");
-const date = document.querySelector("#date");
-const amount = document.querySelector("#amount");
-const expense = document.querySelector("#expense");
-const remaining = document.querySelector(".remaining");
-const deleteBtn = document.querySelector(".fa-trash");
-ekle.addEventListener("click", () => {
-  document.querySelector(".salary").textContent = salary.value;
-});
-kaydet.addEventListener("click", () => {
-    document.querySelector(".container-4-2").style.visibility = "visible"
-  document.querySelector(".date").textContent = date.value;
-  document.querySelector(".harcama").textContent = expense.value;
-  document.querySelector(".amount").textContent = amount.value;
-  document.querySelector(".container-4-2").style.backgroundColor =
-    "rgba(128, 128, 128, 0.901)";
-  document.querySelector(".expenses").textContent = amount.value;
-  remaining.textContent = salary.value - amount.value;
-  if (salary.value > amount.value) {
-    remaining.style.color = "green";
-  } else if (salary.value === amount.value) {
-    remaining.style.color = "orange";
-    remaining.style.fontWeight = "bolder";
-  } else {
-    remaining.style.color = "red";
-  }
-});
-deleteBtn.addEventListener("click", () => {
-  document.querySelector(".container-4-2").style.visibility = "hidden"
-});
 
+// selectors
+const ekleBtn = document.getElementById('ekle-btn')
+const gelirInput = document.getElementById('gelir-input')
+const ekleFormu = document.getElementById('ekle-formu')
 
+// variables
+let gelirler = 0
 
+// ekle formu
+
+ekleFormu.addEventListener('submit', (e)=> {
+  e.preventDefault()
+  gelirler += Number(gelirInput.value)
+  console.log(gelirler)
+  ekleFormu.reset()
+})
